c071fcf06e91923af77778251a976c09cf0c8075
Шемякина Дарья Ильинична <shedi@kontur>
2022-05-01 01:51:10 +0500

    меню

---
+++ Assets/MenuController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+	public void StartGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
